3b89ee9eaa744559c90bdc024c6983f203986e87
Hassansin <mhkr63@gmail.com>
2016-07-08 16:48:10 +0000

    added Tag api

---
+++ test/chartmogul/enrichment/tag.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const ChartMogul = require('../../../lib/chartmogul');
+const config = new ChartMogul.Config('49d974a13658245259899b955ebfba90', 'b009838a8e690d821ea3b76657a6cc58');
+const expect = require('chai').expect;
+const nock = require('nock');
+const Tag = ChartMogul.Enrichment.Tag;
+
+describe('Tag', () => {
+  it('should add tags to a customer', () => {
+    const customerUuid = 'cus_9bf6482d-01e5-4944-957d-5bc730d2cda3';
+    const postBody = {
+      'tags': ['important', 'Prio1']
+    };
+
+    nock(config.API_BASE)
+      .post(`/v1/customers/${customerUuid}/attributes/tags`, postBody)
+      .reply(200, {
+        /* eslint-disable camelcase */
+        tags: [ 'important', 'Prio1' ]
+        /* eslint-enable camelcase */
+      });
+
+    return Tag.add(config, customerUuid, postBody)
+    .then(res => {
+      expect(res).to.have.property('tags');
+    });
+  });
+
+  it('should add tags to customers with email', () => {
+    const postBody = {
+      'email': 'adam@smith.com',
+      'tags': [ 'engage', 'unit loss' ]
+    };
+
+    nock(config.API_BASE)
+      .post('/v1/customers/attributes/tags', postBody)
+      .reply(200, {
+        /* eslint-disable camelcase */
+        'entries': [
+          {
+            'id': 244461,
+            'uuid': 'cus_9bf6482d-01e5-4944-957d-5bc730d2cda3',
+            'external_id': 'cus_0002',
+            'name': 'Adam Smith',
+            'email': 'adam@smith.com',
+            'status': 'Cancelled',
+            'customer-since': '2015-11-01T00:00:00+00:00',
+            'attributes': {
+              'tags': [
+                'foo',
+                'bar',
+                'engage',
+                'unit loss'
+              ],
+              'custom': {
+                'channel': 'Facebook',
+                'age': 8,
+                'CAC': 213
+              }
+            }
+          }
+        ]
+        /* eslint-enable camelcase */
+      });
+
+    return Tag.add(config, postBody)
+    .then(res => {
+      expect(res).to.have.property('entries');
+    });
+  });
+
+  it('should remove tags from a customer', () => {
+    const customerUuid = 'cus_9bf6482d-01e5-4944-957d-5bc730d2cda3';
+    const postBody = {
+      'tags': ['Prio1']
+    };
+
+    nock(config.API_BASE)
+    .delete(`/v1/customers/${customerUuid}/attributes/tags`, postBody)
+    .reply(200, {
+      /* eslint-disable camelcase */
+      tags: [ 'foo', 'bar', 'important', 'engage', 'unit loss' ]
+      /* eslint-enable camelcase */
+    });
+
+    return Tag.remove(config, customerUuid, postBody)
+    .then(res => {
+      expect(res).to.have.property('tags');
+      expect(res.tags).to.not.have.property('Prio1');
+    });
+  });
+});
