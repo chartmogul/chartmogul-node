1982df25e865f067d5cf138065bfc8eb51cdea15
hassansin <rezatxe@gmail.com>
2018-07-30 10:09:53 +0600

    Removed default parameter value to support older versions [ch3381]

--- test/chartmogul/resource.js
+++ test/chartmogul/resource.js
@@ -32,12 +32,13 @@ describe('Resource', () => {
     422: 'ResourceInvalidError',
     500: 'ChartMogulError'
   };
-  Object.keys(errorCodes).forEach(function (code) {
+  Object.keys(errorCodes).forEach(function(code) {
     it(`should throw ${errorCodes[code]}`, done => {
       nock(config.API_BASE)
         .get('/')
         .reply(code, 'error message');
-      if (code >= 500) { // retried request
+      if (code >= 500) {
+        // retried request
         nock(config.API_BASE)
           .get('/')
           .reply(code, 'error message');
@@ -54,7 +55,7 @@ describe('Resource', () => {
     });
   });
 
-  Object.keys(errorCodes).forEach(function (code) {
+  Object.keys(errorCodes).forEach(function(code) {
     it(`should throw ${errorCodes[code]} in callback`, done => {
       nock(config.API_BASE)
         .get('/')
@@ -102,8 +103,9 @@ describe('Resource', () => {
         expect(e).to.be.instanceOf(ChartMogul.ResourceInvalidError);
         expect(e.httpStatus).to.equal(422);
         expect(e.response.error).to.equal('message');
-        expect(e.message)
-          .to.equal('The Customer  could not be created or updated. {"error":"message"}');
+        expect(e.message).to.equal(
+          'The Customer  could not be created or updated. {"error":"message"}'
+        );
         done();
       });
   });
@@ -120,7 +122,11 @@ describe('Resource', () => {
 
 describe('Resource Retry', () => {
   const port = 12345;
-  const config = new ChartMogul.Config('token', 'secret', `http://localhost:${port}`);
+  const config = new ChartMogul.Config(
+    'token',
+    'secret',
+    `http://localhost:${port}`
+  );
   let server, status, retry;
 
   before(done => {
@@ -131,33 +137,31 @@ describe('Resource Retry', () => {
   it('should retry if status 429 is received', () => {
     retry = 1;
     status = 429;
-    return Customer.all(config)
-      .then(res => {
-        expect(res).to.be.eql('ok');
-        expect(retry).to.be.eql(0);
-      });
+    return Customer.all(config).then(res => {
+      expect(res).to.be.eql('ok');
+      expect(retry).to.be.eql(0);
+    });
   });
   it('should retry if status 500 is received', () => {
     retry = 1;
     status = 500;
-    return Customer.all(config)
-      .then(res => {
-        expect(res).to.be.eql('ok');
-        expect(retry).to.be.eql(0);
-      });
+    return Customer.all(config).then(res => {
+      expect(res).to.be.eql('ok');
+      expect(retry).to.be.eql(0);
+    });
   });
 
   it('should retry on ECONNRESET', () => {
     retry = 1;
     status = 0;
-    return Customer.all(config)
-      .then(res => {
-        expect(res).to.be.eql('ok');
-        expect(retry).to.be.eql(0);
-      });
+    return Customer.all(config).then(res => {
+      expect(res).to.be.eql('ok');
+      expect(retry).to.be.eql(0);
+    });
   });
 
-  function startMockServer (cb = () => {}) {
+  function startMockServer(cb) {
+    cb = cb || function() {};
     const server = http.createServer((req, res) => {
       if (retry <= 0) {
         return res.end('ok');
@@ -174,4 +178,3 @@ describe('Resource Retry', () => {
     return server;
   }
 });
-
