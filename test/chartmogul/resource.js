d0bba7f2337bba72a39eb94b2b9518c49e730e6b
Hassansin <rezatxe@gmail.com>
2018-07-31 14:54:54 +0600

    Add exponential backoff retry [ch3381] (#33)
    * Added retry logic [ch3381]
    
    * Removed default parameter value to support older versions [ch3381]
    
    * Added comments [ch3381]
    
    * Bumped version, Updated README, disabling backoff [ch3381]
    
    * Removed zero checking [ch3381]

--- test/chartmogul/resource.js
+++ test/chartmogul/resource.js
@@ -2,13 +2,14 @@
 
 const expect = require('chai').expect;
 const nock = require('nock');
+const http = require('http');
 const ChartMogul = require('../../lib/chartmogul');
 const Resource = require('../../lib/chartmogul/resource');
 const Customer = require('../../lib/chartmogul/customer');
 
-const config = new ChartMogul.Config('token', 'secret');
-
 describe('Resource', () => {
+  const config = new ChartMogul.Config('token', 'secret');
+  config.retries = 0; // no retry
   it('should send basicAuth headers', done => {
     nock(config.API_BASE)
       .get('/')
@@ -91,8 +92,9 @@ describe('Resource', () => {
         expect(e).to.be.instanceOf(ChartMogul.ResourceInvalidError);
         expect(e.httpStatus).to.equal(422);
         expect(e.response.error).to.equal('message');
-        expect(e.message)
-          .to.equal('The Customer  could not be created or updated. {"error":"message"}');
+        expect(e.message).to.equal(
+          'The Customer  could not be created or updated. {"error":"message"}'
+        );
         done();
       });
   });
@@ -105,4 +107,66 @@ describe('Resource', () => {
         done();
       });
   });
+});
+
+describe('Resource Retry', () => {
+  const port = 12345;
+  const config = new ChartMogul.Config(
+    'token',
+    'secret',
+    `http://localhost:${port}`
+  );
+  let server, status, retry;
+
+  before(done => {
+    server = startMockServer(done);
+  });
+  after(done => server.close(done));
+
+  it('should retry if status 429 is received', () => {
+    retry = 1;
+    status = 429;
+    return Customer.all(config).then(res => {
+      expect(res).to.be.eql('ok');
+      expect(retry).to.be.eql(0);
+    });
+  });
+  it('should retry if status 500 is received', () => {
+    retry = 1;
+    status = 500;
+    return Customer.all(config).then(res => {
+      expect(res).to.be.eql('ok');
+      expect(retry).to.be.eql(0);
+    });
+  });
+
+  it('should retry on ECONNRESET', () => {
+    retry = 1;
+    status = 0;
+    return Customer.all(config).then(res => {
+      expect(res).to.be.eql('ok');
+      expect(retry).to.be.eql(0);
+    });
+  });
+
+  // mock server used to send server errors to let the client retry
+  // it will send several 5xx status and finally sends a 200 status
+  // so clients can retry on 5xx status codes
+  function startMockServer (cb) {
+    cb = cb || function () {};
+    const server = http.createServer((req, res) => {
+      if (retry <= 0) {
+        return res.end('ok');
+      }
+      if (status === 0 && retry >= 0) {
+        retry--;
+        return req.socket.destroy(new Error('some error'));
+      }
+      retry--;
+      res.writeHead(status);
+      res.end(status.toString());
+    });
+    server.listen(port, cb);
+    return server;
+  }
 });
