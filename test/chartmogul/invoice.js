c6383da000d1d494d367d298a43165af0b1b9019
Sebastian Brych <sebastian+github@nwscx.com>
2021-11-05 10:30:01 +0100

    Run npm install & eslint

--- test/chartmogul/invoice.js
+++ test/chartmogul/invoice.js
@@ -240,7 +240,7 @@ describe('Invoices', () => {
   });
 
   it('should retrieve an invoice', () => {
-    var payload = { external_id: 'some_invoice_id' };
+    const payload = { external_id: 'some_invoice_id' };
     nock(config.API_BASE)
       .get('/v1/invoices/inv_cff3a63c-3915-435e-a675-85a8a8ef4454')
       .reply(200, payload);
