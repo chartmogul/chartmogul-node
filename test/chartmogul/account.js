ee1833c584dcf7d3db5d9e33c24cd4d32bde00c2
Ahmed Galadima <Ahmed.Galadima@hotmail.com>
2024-04-05 15:21:59 +0200

    Fix assertion for account test

--- test/chartmogul/account.js
+++ test/chartmogul/account.js
@@ -7,26 +7,22 @@ const nock = require('nock');
 const Account = ChartMogul.Account;
 
 describe('Account', () => {
-  it('should retrieve the details of current account', () => {
+  it('should retrieve the details of current account', async () => {
     nock(config.API_BASE)
       .get('/v1/account')
       .reply(200, {
-      /* eslint-disable camelcase */
+        /* eslint-disable camelcase */
         name: 'Example Test Company',
         currency: 'EUR',
         time_zone: 'Europe/Berlin',
         week_start_on: 'sunday'
-      /* eslint-enable camelcase */
+        /* eslint-enable camelcase */
       });
 
-    return Account.retrieve(config, (err, res) => {
-      if (err) {
-        return err;
-      }
-      expect(res.name).to.be.equal('Example Test Company');
-      expect(res.currency).to.be.equal('EUR');
-      expect(res.time_zone).to.be.equal('Europe/Berlin');
-      expect(res.week_start_on).to.be.equal('sunday');
-    });
+    const account = await Account.retrieve(config);
+    expect(account.name).to.be.equal('Example Test Company');
+    expect(account.currency).to.be.equal('EUR');
+    expect(account.time_zone).to.be.equal('Europe/Berlin');
+    expect(account.week_start_on).to.be.equal('sunday');
   });
 });
