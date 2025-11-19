407b885f21ea1b4f7bbca5763c39b6b666ae9ca8
Srikala Bharadwaj <srikala@chartmogul.com>
2025-11-19 18:36:50 +0530

    Add to test [ana-174]

--- test/chartmogul/metrics/activity.js
+++ test/chartmogul/metrics/activity.js
@@ -48,6 +48,7 @@ describe('Activity', () => {
             type: 'new_biz',
             currency: 'USD',
             'subscription-external-id': 'sub_2',
+            'subscription-set-external-id': 'sub_2',
             'plan-external-id': '11',
             'customer-name': 'customer_2',
             'customer-uuid': '8bc55ab6-c3b5-11eb-ac45-2f9a49d75af7',
