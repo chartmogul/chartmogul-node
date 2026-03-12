b9612185d0ec0ccbf328e9709dcd30d3c5306259
Keith Dawson <keith@chartmogul.com>
2026-03-12 19:57:01 +0900

    [del-1607] Added tests for inclusion of 'external_id' field in Contact model requests

--- test/chartmogul/contact.js
+++ test/chartmogul/contact.js
@@ -13,6 +13,7 @@ describe('Contact', () => {
       customer_uuid: 'cus_919d5d7c-9e23-11ed-a936-97fbf69ba02b',
       data_source_uuid: 'ds_87832fac-ab61-11ec-a8d8-6fb18044a151',
       data_source_customer_external_id: 'scus_606e14228cff7d9db01be55f4e32e5e4',
+      external_id: 'contact_external_id_001',
       first_name: 'First name',
       last_name: 'Last name',
       position: 9,
@@ -37,6 +38,7 @@ describe('Contact', () => {
         customer_uuid: 'cus_00000000-0000-0000-0000-000000000000',
         data_source_uuid: 'ds_00000000-0000-0000-0000-000000000000',
         data_source_customer_external_id: 'external_001',
+        external_id: 'contact_external_id_001',
         first_name: 'First name',
         last_name: 'Last name',
         position: 9,
@@ -55,6 +57,109 @@ describe('Contact', () => {
 
     const contact = await Contact.create(config, postBody);
     expect(contact).to.have.property('uuid');
+  });
+
+  it('creates a new contact with external_id as null', async () => {
+    const postBody = {
+      /* eslint-disable camelcase */
+      customer_uuid: 'cus_919d5d7c-9e23-11ed-a936-97fbf69ba02b',
+      data_source_uuid: 'ds_87832fac-ab61-11ec-a8d8-6fb18044a151',
+      data_source_customer_external_id: 'scus_606e14228cff7d9db01be55f4e32e5e4',
+      external_id: null,
+      first_name: 'First name',
+      last_name: 'Last name',
+      position: 9,
+      title: 'Title',
+      email: 'test@example.com',
+      phone: '+1234567890',
+      linked_in: 'https://linkedin.com/not_found',
+      twitter: 'https://twitter.com/not_found',
+      notes: 'Heading\nBody\nFooter',
+      custom: [
+        { key: 'Booleanz', value: false },
+        { key: 'MyIntegerAttribute', value: 123 }
+      ]
+      /* eslint-enable camelcase */
+    };
+
+    nock(config.API_BASE)
+      .post('/v1/contacts', postBody)
+      .reply(200, {
+        /* eslint-disable camelcase */
+        uuid: 'con_00000000-0000-0000-0000-000000000000',
+        customer_uuid: 'cus_00000000-0000-0000-0000-000000000000',
+        data_source_uuid: 'ds_00000000-0000-0000-0000-000000000000',
+        data_source_customer_external_id: 'external_001',
+        external_id: null,
+        first_name: 'First name',
+        last_name: 'Last name',
+        position: 9,
+        title: 'Title',
+        email: 'test@example.com',
+        phone: '+1234567890',
+        linked_in: 'https://linkedin.com/not_found',
+        twitter: 'https://twitter.com/not_found',
+        notes: 'Heading\nBody\nFooter',
+        custom: {
+          MyStringAttribute: 'Test',
+          MyIntegerAttribute: 123
+        }
+        /* eslint-enable camelcase */
+      });
+
+    const contact = await Contact.create(config, postBody);
+    expect(contact.external_id).to.equal(null);
+  });
+
+  it('creates a new contact without external_id', async () => {
+    const postBody = {
+      /* eslint-disable camelcase */
+      customer_uuid: 'cus_919d5d7c-9e23-11ed-a936-97fbf69ba02b',
+      data_source_uuid: 'ds_87832fac-ab61-11ec-a8d8-6fb18044a151',
+      data_source_customer_external_id: 'scus_606e14228cff7d9db01be55f4e32e5e4',
+      first_name: 'First name',
+      last_name: 'Last name',
+      position: 9,
+      title: 'Title',
+      email: 'test@example.com',
+      phone: '+1234567890',
+      linked_in: 'https://linkedin.com/not_found',
+      twitter: 'https://twitter.com/not_found',
+      notes: 'Heading\nBody\nFooter',
+      custom: [
+        { key: 'Booleanz', value: false },
+        { key: 'MyIntegerAttribute', value: 123 }
+      ]
+      /* eslint-enable camelcase */
+    };
+
+    nock(config.API_BASE)
+      .post('/v1/contacts', postBody)
+      .reply(200, {
+        /* eslint-disable camelcase */
+        uuid: 'con_00000000-0000-0000-0000-000000000000',
+        customer_uuid: 'cus_00000000-0000-0000-0000-000000000000',
+        data_source_uuid: 'ds_00000000-0000-0000-0000-000000000000',
+        data_source_customer_external_id: 'external_001',
+        external_id: null,
+        first_name: 'First name',
+        last_name: 'Last name',
+        position: 9,
+        title: 'Title',
+        email: 'test@example.com',
+        phone: '+1234567890',
+        linked_in: 'https://linkedin.com/not_found',
+        twitter: 'https://twitter.com/not_found',
+        notes: 'Heading\nBody\nFooter',
+        custom: {
+          MyStringAttribute: 'Test',
+          MyIntegerAttribute: 123
+        }
+        /* eslint-enable camelcase */
+      });
+
+    const contact = await Contact.create(config, postBody);
+    expect(contact.external_id).to.equal(null);
   });
 
   it('should list all contacts with pagination', async () => {
@@ -118,6 +223,7 @@ describe('Contact', () => {
         customer_uuid: 'cus_00000000-0000-0000-0000-000000000000',
         data_source_uuid: 'ds_00000000-0000-0000-0000-000000000000',
         customer_external_id: 'external_001',
+        external_id: 'contact_external_id_001',
         first_name: 'First name',
         last_name: 'Last name',
         position: 9,
@@ -142,23 +248,47 @@ describe('Contact', () => {
     const contactUuid = 'con_00000000-0000-0000-0000-000000000000';
 
     /* eslint-disable camelcase */
-    const postBody = { email: 'test2@example.com' };
+    const patchBody = { email: 'test2@example.com', external_id: 'contact_external_id_002' };
     /* eslint-enable camelcase */
 
     nock(config.API_BASE)
-      .patch(`/v1/contacts/${contactUuid}`, postBody)
+      .patch(`/v1/contacts/${contactUuid}`, patchBody)
       .reply(200, {
         /* eslint-disable camelcase */
         uuid: contactUuid,
         customer_uuid: 'cus_00000000-0000-0000-0000-000000000000',
         data_source_uuid: 'ds_00000000-0000-0000-0000-000000000000',
         customer_external_id: 'external_001',
-        email: 'test2@example.com'
+        email: 'test2@example.com',
+        external_id: 'contact_external_id_002'
         /* eslint-enable camelcase */
       });
 
-    const contact = await Contact.modify(config, contactUuid, postBody);
+    const contact = await Contact.modify(config, contactUuid, patchBody);
     expect(contact.email).to.be.equal('test2@example.com');
+    expect(contact.external_id).to.be.equal('contact_external_id_002');
+  });
+
+  it('updates a contact with external_id as null', async () => {
+    const contactUuid = 'con_00000000-0000-0000-0000-000000000000';
+
+    /* eslint-disable camelcase */
+    const patchBody = { external_id: null };
+    /* eslint-enable camelcase */
+
+    nock(config.API_BASE)
+      .patch(`/v1/contacts/${contactUuid}`, patchBody)
+      .reply(200, {
+        /* eslint-disable camelcase */
+        uuid: contactUuid,
+        customer_uuid: 'cus_00000000-0000-0000-0000-000000000000',
+        data_source_uuid: 'ds_00000000-0000-0000-0000-000000000000',
+        external_id: null
+        /* eslint-enable camelcase */
+      });
+
+    const contact = await Contact.modify(config, contactUuid, patchBody);
+    expect(contact.external_id).to.equal(null);
   });
 
   it('deletes a contact', async () => {
